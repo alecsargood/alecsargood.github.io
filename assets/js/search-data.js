c6ec1b514ef62bb22993c915f4c1746160bbd2cc
alecsargood <58740397+alecsargood@users.noreply.github.com>
2026-02-23 12:07:25 +0000

    Deploying to gh-pages from @ alecsargood/alecsargood.github.io@89782fc2bcdb536c70d9a5b2b1c9d0c3169b0330 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "news-excited-to-share-that-cocolit-controlnet-conditioned-latent-image-translation-for-mri-to-amyloid-pet-synthesis-has-been-accepted-at-aaai-2026-this-is-joint-first-author-work-with-l-puglisi",
           title: 'Excited to share that CoCoLIT: ControlNet-Conditioned Latent Image Translation for MRI to Amyloid...',
           description: "",
